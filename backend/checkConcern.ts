d02f686572464b1255456b143742468161805b6b
Ragul000 <vraguldharun@gmail.com>
2026-07-31 17:39:21 +0530

    chore: remove unwanted dummy data scripts and testing files

--- backend/checkConcern.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-  const concern = await prisma.concern.findFirst({ where: { active: 1, isDeleted: false } });
-  console.log("Concern:", concern);
-}
-main().finally(() => prisma.$disconnect());
--- backend/checkParty.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-  const party = await prisma.party.findFirst({ where: { active: 1, isDeleted: false } });
-  console.log("Party:", party);
-}
-main().finally(() => prisma.$disconnect());
